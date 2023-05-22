0855793c95ded9958d16d1b70e080e11ac47788e
Caleb Widmer <Calebwidmer@gmail.com>
2023-05-22 15:51:58 -0400

    end of lesson 19. Understanding NgRx Effects - A Simple Example

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,6 +11,7 @@ import {AuthService} from "./auth.service";
 import { authReducer } from './reducers';
 import { AuthGuard } from './auth.guard';
 import { EffectsModule } from '@ngrx/effects';
+import { AuthEffects } from './auth.effects';
 @NgModule({
     imports: [
         CommonModule,
@@ -20,7 +21,7 @@ import { EffectsModule } from '@ngrx/effects';
         MatButtonModule,
         RouterModule.forChild([{path: '', component: LoginComponent}]),
         StoreModule.forFeature('auth', authReducer),
-        EffectsModule.forFeature([])
+        EffectsModule.forFeature([AuthEffects])
     ],
     declarations: [LoginComponent],
     exports: [LoginComponent]
