9277c23bc5b77a181ccb873934c2bd2889cb50ae
Caleb Widmer <Calebwidmer@gmail.com>
2023-06-03 14:12:58 -0400

    34. Optimistically Editing Entity Data - The Edit Course Dialog

--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -4,6 +4,9 @@ import {Course} from '../model/course';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {CoursesHttpService} from '../services/courses-http.service';
+import { AppState } from '../../reducers';
+import { Store } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
 
 @Component({
   selector: 'course-dialog',
@@ -26,7 +29,7 @@ export class EditCourseDialogComponent {
     private fb: FormBuilder,
     private dialogRef: MatDialogRef<EditCourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data,
-    private coursesService: CoursesHttpService) {
+    private store: Store<AppState>) {
 
     this.dialogTitle = data.dialogTitle;
     this.course = data.course;
@@ -63,13 +66,18 @@ export class EditCourseDialogComponent {
       ...this.form.value
     };
 
-    this.coursesService.saveCourse(course.id, course)
-      .subscribe(
-        () => this.dialogRef.close()
-      )
-
+const update: Update<Course> ={
+  id:course.id,
+   changes: course
+};
+this.store.dispatch(courseUpdated({update}));
+this.dialogRef.close();
 
   }
 
 
 }
+function courseUpdated(arg0: { update: import("@ngrx/entity/src/models").UpdateNum<Course>; }): any {
+  throw new Error('Function not implemented.');
+}
+
